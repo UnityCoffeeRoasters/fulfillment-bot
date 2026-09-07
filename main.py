415514ad013741c4148d4b082f74d09322ba4d9e
UnityCoffeeRoasters <info@unitycoffee.pt>
2026-09-07 20:03:25 +0100

    Change health endpoint to support HEAD method

--- main.py
+++ main.py
@@ -201,6 +201,6 @@ async def order_inprogress(
 
 # ── Healthcheck ───────────────────────────────────────────────────────────────
 
-@app.get("/health")
+@app.api_route("/health", methods=["GET", "HEAD"])
 def health():
     return {"status": "alive"}
